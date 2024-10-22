f41a1f1438268edcb8c5572025467647b89bf2ab
Manuel Escareño <bigmander@hotmail.es>
2024-10-22 16:52:29 -0600

    wip

--- back-end/.net/Application/Repositories/CommentsRepository.cs
+++ back-end/.net/Application/Repositories/CommentsRepository.cs
@@ -1,11 +1,17 @@
 ﻿using Domain;
 using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Repositories;
 
 public class CommentsRepository : Repository<Comment>
 {
     public CommentsRepository(ApplicationDbContext context) : base(context)
-    { 
+    {
+
+    }
+    public async override Task<Comment?> GetAsync(Guid id)
+    {
+        return await _entities.Include(c => c.Post).FirstOrDefaultAsync(c => c.Id == id);
     }
 }
